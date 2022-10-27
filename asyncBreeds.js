d4520662cfd4ee560fddb453b86879fe4e8d414e
Sid Natarajan <sid.nat@gmail.com>
2022-10-27 01:20:12 +0000

    Async return values reading excercise

---
+++ asyncBreeds.js
@@ -0,0 +1,16 @@
+// asyncBreeds.js
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+    if (!error) functionToRunWhenThingsAreDone(data);
+  });
+};
+
+const printOutCatBreed = breed => {
+  console.log('Return Value: ', breed); // => print out details correctly.
+};
+
+breedDetailsFromFile('Balinese', printOutCatBreed);
